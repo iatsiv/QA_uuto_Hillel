c97c285b87aa9cd81d1b029319697c8c244565ef
o.yatsiv <e.enion@gmail.com>
2023-05-14 23:54:35 +0300

    less 14 DZ 16 - refactoring

---
+++ Lesson15_test_SELEN_API_DZ17/create_car.py
@@ -0,0 +1,5 @@
+class CraeteCar():
+    def __init__(self, carBrandId, carModelId, mileage):
+        self.carBrandId = carBrandId
+        self.carModelId = carModelId
+        self.mileage = mileage
